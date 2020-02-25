fc14c78ef5c0b13c1aa7536a6cefd02367c39adf
Owen <OwenH@whitehinge.com>
2020-02-25 18:31:41 +0000

    Fucked it

---
+++ MachineLearningTester/MachineLearningTester/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using MachineLearningTesterML.Model;
+
+namespace MachineLearningTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var input = new ModelInput();
+            input.ImageSource = @"C:\MLTestFiles\index.png";
+            var result = ConsumeModel.Predict(input);
+            Console.WriteLine($"This is a {result.Prediction} with a confidence of {Math.Round(Convert.ToDecimal(result.Score.Max())*100m, 3)}%");
+        }
+    }
+}
